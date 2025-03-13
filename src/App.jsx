bc75893cb6fcfa9b7724ba8dbdecda661ec766f1
Mauricio <rubiom831@gmail.com>
2025-03-13 09:05:02 -0800

    Implementación de autenticación y componente de inicio de sesión

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-const App = () => {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./database/authcontext";
+import ProtectedRoute from "./components/ProtectedRoute"; 
+import Login from './views/Login'
+import Encabezado from "./components/Encabezado";
+import Inicio from "./views/Inicio";
+
+import './App.css'
+
+function App() {
+
   return (
-    <div style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      height: '100vh', 
-      textAlign: 'center' 
-    }}>
-      <h1>Componente Principal</h1>
-    </div>
-  );
+    <>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <Encabezado />
+            <main>
+              <Routes>
+                
+                <Route path="/" element={<Login />} />
+                <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
+
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </AuthProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
